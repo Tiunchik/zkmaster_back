155b00159fb023cd06840aac86b76a5e03ec8063
DaniilsLoputevs <55362320+DaniilsLoputevs@users.noreply.github.com>
2020-12-02 01:26:00 +0000

    - add log4j - add stub WatcherImpl
    - change bean config
    
    - todo fix warning about slf4j init

--- src/main/java/org/zkmaster/backend/configuration/ZooConfig.java
+++ src/main/java/org/zkmaster/backend/configuration/ZooConfig.java
@@ -1,36 +1,29 @@
 package org.zkmaster.backend.configuration;
 
-import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.zkmaster.backend.entity.OurDefaultWatcher;
 
 import java.io.IOException;
 
 @Configuration
 public class ZooConfig {
 
-    @Bean(name = "zoo")
-    public ZooKeeper init(Watcher watcher) {
+    @Bean("zoo")
+    public ZooKeeper init(Watcher stubWatcher) {
         try {
-            return new ZooKeeper("localhost:2181", 4000, watcher);
+            return new ZooKeeper("localhost:2181", 4000, stubWatcher);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    @Bean
+    @Bean("stubWatcher")
     public Watcher createWatcher() {
-        return new Watcher() {
-            @Override
-            public void process(WatchedEvent event) {
-                System.out.println(event.toString());
-            }
-        };
+        return new OurDefaultWatcher();
     }
-
 
 }
